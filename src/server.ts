cc7079cf9448972d04db539f3e11969e8328f80d
YeongWoooo <rdd9223@gmail.com>
2020-12-30 15:42:52 +0900

    Init: 초기 프로젝트 세팅 (#2)

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import errorHandler from "errorhandler";
+import app from "./app";
+
+/**
+ * Error Handler. Provides full stack
+ */
+if (process.env.NODE_ENV === "development") {
+  app.use(errorHandler());
+}
+
+/**
+ * Start Express server.
+ */
+const server = app.listen(app.get("port"), () => {
+  console.log(`  App is running at http://localhost:${app.get("port")} in ${app.get("env")} mode`);
+  console.log("  Press CTRL-C to stop\n");
+});
+
+export default server;
