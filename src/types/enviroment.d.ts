3303faa1b159c37b6bd18ffda95e696a5f17619d
YeongWoooo <rdd9223@gmail.com>
2020-12-30 23:23:35 +0900

    Feat: 디렉토리 변경, 리퀘스트 값에 토큰 해석값 선언

---
+++ src/types/enviroment.d.ts
@@ -0,0 +1,25 @@
+// import { User } from '../models/Users';
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production';
+      TOKEN_SECRET_KEY: string;
+      // DATABASE
+      DATABASE_NAME: string;
+      DATABASE_HOST: string;
+      DATABASE_DIALECT: string;
+      DATABASE_USERNAME: string;
+      DATABASE_PASSWORD: string;
+    }
+  }
+  namespace Express {
+    interface Request {
+      decoded?: string;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
