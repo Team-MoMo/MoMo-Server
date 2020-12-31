f13085ae453920bd2364723f5078247ae660a55b
chaeppy <chaeppy108@gmail.com>
2020-12-31 15:13:50 +0900

    Feat: notification model 추가

---
+++ src/models/notification.ts
@@ -0,0 +1,45 @@
+import { Model, DataTypes, Optional } from 'sequelize';
+import { sequelize } from './index';
+
+interface NotificationAttributes {
+  id: number;
+  deviceToken: string;
+  userId: number;
+}
+
+interface NotificationCreationAttributes extends Optional<NotificationAttributes, 'id'> {}
+
+class Notification
+  extends Model<NotificationAttributes, NotificationCreationAttributes>
+  implements NotificationAttributes {
+  public id!: number;
+  public deviceToken!: string;
+  public userId!: number;
+
+  readonly createdAt!: Date;
+  readonly updatedAt!: Date;
+}
+
+Notification.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    deviceToken: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'Notifications',
+  }
+);
+
+export default Notification;
