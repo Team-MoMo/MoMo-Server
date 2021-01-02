05625731cf8c7dfb4f1fe0cf8a4ea14d3eff60cd
yeonghyeonSeok <syh4834@naver.com>
2021-01-02 18:53:17 +0900

    Style: 타입(object) 정의

--- src/controllers/emotions_controller.ts
+++ src/controllers/emotions_controller.ts
@@ -4,7 +4,7 @@ import { emotionsService } from '../services';
 
 export const readAll = async (req: Request, res: Response) => {
   try {
-    const emotions = await emotionsService.readAll();
+    const emotions: object = await emotionsService.readAll();
     return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_READ_ALL_SUCCESS('감정'), emotions));
   } catch (err) {
     return res.status(statusCode.INTERNAL_SERVER_ERROR).send(authUtil.successFalse(resMessage.X_READ_ALL_FAIL('감정')));
