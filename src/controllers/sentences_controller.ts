10bf1e4e7d34a493027d54c8df0b24cca1e4c9f8
yeonghyeonSeok <syh4834@naver.com>
2021-01-02 18:51:04 +0900

    Refactor: query 추가 및 attributes, type 설정

--- src/controllers/sentences_controller.ts
+++ src/controllers/sentences_controller.ts
@@ -1,11 +1,13 @@
 import { statusCode, authUtil, resMessage } from '../utils';
 import express, { Request, Response, NextFunction, Send } from 'express';
 import { sentencesService } from '../services';
+import { QueryTypes } from 'sequelize/types';
 
 export const readAll = async (req: Request, res: Response) => {
   try {
-    const sentences = await sentencesService.readAll();
-    return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_READ_ALL_FAIL('문장'), sentences));
+    const emotion: any = req.query.emotion;
+    const sentences: object = await sentencesService.readAll(emotion);
+    return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_READ_ALL_SUCCESS('문장'), sentences));
   } catch (err) {
     return res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.X_READ_ALL_FAIL('문장')));
   }
--- src/services/sentences_service.ts
+++ src/services/sentences_service.ts
@@ -1,8 +1,13 @@
 import model from '../models';
 
-export const readAll = async () => {
+export const readAll = async (emotionId: any) => {
   try {
-    const sentences = await model.Sentence.findAll();
+    const sentences: object = await model.Sentence.findAll({
+      attributes: ['id', 'contents', 'writer', 'publisher', 'emotionId'],
+      where: {
+        emotionId,
+      },
+    });
     return sentences;
   } catch (err) {
     throw err;
