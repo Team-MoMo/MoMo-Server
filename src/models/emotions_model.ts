943fbf989d0fbd3801bb1b5d5b2da471f58b0fbc
YeongWoooo <rdd9223@gmail.com>
2021-01-02 15:24:50 +0900

    Fix: 오타 수정

--- src/models/emotions_model.ts
+++ src/models/emotions_model.ts
@@ -13,6 +13,12 @@ class Emotion extends Model<EmotionAttributes> implements EmotionAttributes {
   readonly createdAt!: Date;
   readonly updatedAt!: Date;
 
+<<<<<<< HEAD
+=======
+  getSentence!: HasManyGetAssociationsMixin<Sentence>;
+  public readonly sentences?: Sentence[];
+
+>>>>>>> Fix: 오타 수정
   static associations: {
     diaries: Association<Emotion, Diary>;
   };
