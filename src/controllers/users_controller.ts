d4e899fe29f2966ecf0647437ba5547af2c539b1
chaeppy <chaeppy108@gmail.com>
2021-01-04 02:20:06 +0900

    Feat: 로그인,회원가입 컨트롤러

--- src/controllers/users_controller.ts
+++ src/controllers/users_controller.ts
@@ -1,12 +1,30 @@
 import { statusCode, authUtil, resMessage } from '../utils';
 import { Request, Response } from 'express';
+import { jwt } from '../utils';
 import { usersService } from '../services';
 
 export const signup = async (req: Request, res: Response) => {
-  const {} = req.body;
+  const { email, password, name }: { email: string; password: string; name: string } = req.body;
+
+  if (!email || !password || !name) {
+    return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NULL_VALUE));
+  }
 
   try {
-  } catch (err) {}
+    const user = await usersService.readOneByEmail(email);
+    if (user) {
+      return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.DUPLICATE_ID));
+    }
+
+    const newUser = await usersService.signup(email, password, name);
+    const { token } = jwt.sign(newUser);
+    newUser.password = '';
+    newUser.passwordSalt = '';
+
+    return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.SIGN_UP_SUCCESS, { user: newUser, token }));
+  } catch (err) {
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.SIGN_UP_FAIL));
+  }
 };
 
 export const signupByApple = async (req: Request, res: Response) => {
@@ -24,10 +42,30 @@ export const signupByKakao = async (req: Request, res: Response) => {
 };
 
 export const signin = async (req: Request, res: Response) => {
-  const {} = req.body;
+  const { email, password }: { email: string; password: string } = req.body;
+
+  if (!email || !password) {
+    return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NULL_VALUE));
+  }
 
   try {
-  } catch (err) {}
+    const user = await usersService.readOneByEmail(email);
+    if (user == null) {
+      return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.MISS_MATCH_USER_INFO));
+    }
+
+    if (!usersService.signin(user, password)) {
+      return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.MISS_MATCH_USER_INFO));
+    }
+
+    const { token } = jwt.sign(user);
+    user.password = '';
+    user.passwordSalt = '';
+
+    return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.SIGN_IN_SUCCESS, { user, token }));
+  } catch (error) {
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.SIGN_IN_FAIL));
+  }
 };
 
 export const readAll = async (req: Request, res: Response) => {
