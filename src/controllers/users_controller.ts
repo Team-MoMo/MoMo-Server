d952994ca16001ec49bc491ea88b24e7a77317a5
yeonghyeonSeok <syh4834@naver.com>
2021-01-04 23:00:17 +0900

    Style: 코드리뷰 반영  최종

--- src/controllers/users_controller.ts
+++ src/controllers/users_controller.ts
@@ -1,6 +1,7 @@
 import { statusCode, authUtil, resMessage } from '../utils';
 import { Request, Response } from 'express';
 import { usersService } from '../services';
+import User from '../models/users_model';
 
 export const signup = async (req: Request, res: Response) => {
   const {} = req.body;
@@ -33,9 +34,9 @@ export const signin = async (req: Request, res: Response) => {
 export const readAll = async (req: Request, res: Response) => {
   try {
     const users = await usersService.readAll();
-    res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_READ_ALL_SUCCESS('회원'), users));
+    return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_READ_ALL_SUCCESS('회원'), users));
   } catch (err) {
-    res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.X_READ_ALL_FAIL('회원')));
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.X_READ_ALL_FAIL('회원')));
   }
 };
 
@@ -45,12 +46,11 @@ export const readOne = async (req: Request, res: Response) => {
   try {
     const userInfo = await usersService.readOne(id);
     if (!userInfo) {
-      res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NO_X('회원')));
-    } else {
-      res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_READ_SUCCESS('회원'), userInfo));
+      return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NO_X('회원')));
     }
+    return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_READ_SUCCESS('회원'), userInfo));
   } catch (err) {
-    res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.X_READ_FAIL('회원')));
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.X_READ_FAIL('회원')));
   }
 };
 
@@ -59,23 +59,23 @@ export const updateAlarm = async (req: Request, res: Response) => {
   const { isAlarmSet, alarmTime } = req.body;
 
   if (!id || !isAlarmSet) {
-    res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NULL_VALUE));
+    return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NULL_VALUE));
   }
   try {
     const userInfo = await usersService.readOne(id);
     if (!userInfo) {
-      res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NO_X('회원')));
-    } else {
-      let userAlarmInfo;
-      if (!alarmTime) {
-        userAlarmInfo = await usersService.updateAlarmSet(id, isAlarmSet);
-      } else {
-        userAlarmInfo = await usersService.updateAlarmTime(id, isAlarmSet, alarmTime);
-      }
-      res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_UPDATE_SUCCESS('알람'), userAlarmInfo));
+      return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NO_X('회원')));
     }
+
+    let userAlarmInfo;
+    if (!alarmTime) {
+      userAlarmInfo = await usersService.updateAlarmSet(id, isAlarmSet);
+    } else {
+      userAlarmInfo = await usersService.updateAlarmTime(id, isAlarmSet, alarmTime);
+    }
+    return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_UPDATE_SUCCESS('알람'), userAlarmInfo));
   } catch (err) {
-    res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.X_UPDATE_FAIL('알람')));
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.X_UPDATE_FAIL('알람')));
   }
 };
 
@@ -84,23 +84,21 @@ export const checkPassword = async (req: Request, res: Response) => {
   const { password } = req.body;
 
   if (!id || !password) {
-    res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NULL_VALUE));
+    return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NULL_VALUE));
   }
 
   try {
     const userInfo = await usersService.readOne(id);
     if (!userInfo) {
-      res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NO_X('회원')));
-    } else {
-      const checkPasswordResult = await usersService.checkPassword(userInfo, password);
-      if (!checkPasswordResult) {
-        res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.MISS_MATCH_PASSWORD));
-      } else {
-        res.status(statusCode.OK).json(authUtil.successTrue(resMessage.MATCH_PASSWORD));
-      }
+      return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NO_X('회원')));
     }
+    const checkPasswordResult = await usersService.checkPassword(userInfo, password);
+    if (!checkPasswordResult) {
+      return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.MISS_MATCH_PASSWORD));
+    }
+    return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.MATCH_PASSWORD));
   } catch (err) {
-    res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.INTERNAL_SERVER_ERROR));
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.INTERNAL_SERVER_ERROR));
   }
 };
 
@@ -109,36 +107,39 @@ export const updatePassword = async (req: Request, res: Response) => {
   const { newPassword } = req.body;
 
   if (!id || !newPassword) {
-    res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NULL_VALUE));
+    return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NULL_VALUE));
   }
   try {
     const userInfo = await usersService.readOne(id);
     if (!userInfo) {
-      res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NO_X('회원')));
-    } else {
-      await usersService.updatePassword(id, newPassword);
-      res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_UPDATE_SUCCESS('비밀번호')));
+      return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NO_X('회원')));
     }
+    await usersService.updatePassword(id, newPassword);
+    userInfo.password = '';
+    userInfo.passwordSalt = '';
+    return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_UPDATE_SUCCESS('비밀번호'), userInfo));
   } catch (err) {
-    res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.X_UPDATE_FAIL('비밀번호')));
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .json(authUtil.successFalse(resMessage.X_UPDATE_FAIL('비밀번호')));
   }
 };
 
 export const deleteOne = async (req: Request, res: Response) => {
   const { id } = req.params;
   if (!id) {
-    res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NULL_VALUE));
-  } else {
-    try {
-      const userInfo = await usersService.readOne(id);
-      if (!userInfo) {
-        res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NO_X('회원')));
-      } else {
-        await usersService.deleteOne(userInfo);
-        res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_DELETE_SUCCESS('회원'), userInfo));
-      }
-    } catch (err) {
-      res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.X_DELETE_FAIL('회원')));
+    return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NULL_VALUE));
+  }
+  try {
+    const userInfo = await usersService.readOne(id);
+    if (!userInfo) {
+      return res.status(statusCode.BAD_REQUEST).json(authUtil.successFalse(resMessage.NO_X('회원')));
     }
+    await userInfo.destroy();
+    userInfo.password = '';
+    userInfo.passwordSalt = '';
+    return res.status(statusCode.OK).json(authUtil.successTrue(resMessage.X_DELETE_SUCCESS('회원'), userInfo));
+  } catch (err) {
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).json(authUtil.successFalse(resMessage.X_DELETE_FAIL('회원')));
   }
 };
