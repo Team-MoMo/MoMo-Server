c4b86e4f4678c4039766e5b6f3614d47d606d535
chaeppy <chaeppy108@gmail.com>
2021-01-04 16:52:56 +0900

    Style: 변수명 변경, hashPassword 함수 삭제

--- src/services/users_service.ts
+++ src/services/users_service.ts
@@ -1,11 +1,11 @@
 import crypto from 'crypto';
-import db from '../models';
+import model from '../models';
 import User from '../models/users_model';
 
 export const signup = async (email: string, name: string, password: string) => {
   const salt = crypto.randomBytes(64).toString('base64');
-  const hashedPassword = hashPassword(password, salt);
-  const user = await db.User.create({
+  const hashedPassword = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64');
+  const user = await model.User.create({
     email,
     name,
     password: hashedPassword,
@@ -14,8 +14,12 @@ export const signup = async (email: string, name: string, password: string) => {
   return user;
 };
 
+(password: string, salt: string) => {
+  return;
+};
+
 export const signin = (user: User, password: string) => {
-  const hashedPassword = hashPassword(password, user.passwordSalt);
+  const hashedPassword = crypto.pbkdf2Sync(password, user.passwordSalt, 10000, 64, 'sha512').toString('base64');
   if (hashedPassword != user.password) {
     return false;
   }
@@ -32,14 +36,6 @@ export const signupByKakao = async () => {
   } catch (err) {}
 };
 
-export const hashPassword = (password: string, salt: string) => {
-  try {
-    return crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('base64');
-  } catch (err) {
-    throw err;
-  }
-};
-
 export const readAll = async () => {
   try {
     console.log('success'); //라우팅 테스트용
@@ -52,7 +48,7 @@ export const readOne = async () => {
 };
 
 export const readOneByEmail = async (email: string) => {
-  const user = await db.User.findOne({
+  const user = await model.User.findOne({
     where: { email },
   });
   return user;
