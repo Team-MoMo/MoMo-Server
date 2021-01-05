bc78535bd6a390d78b5e0f83c66206e75632df97
YeongWoooo <rdd9223@gmail.com>
2021-01-05 20:41:39 +0900

    Feat: 책 이름 컬럼 추가

--- src/models/sentences_model.ts
+++ src/models/sentences_model.ts
@@ -45,6 +45,10 @@ Sentence.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    writer: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     publisher: {
       type: DataTypes.STRING,
       allowNull: false,
