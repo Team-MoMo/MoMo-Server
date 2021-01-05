d129daf186edcb66066cfdc5e18a3e94e8df39e3
yeonghyeonSeok <syh4834@naver.com>
2021-01-05 23:10:20 +0900

    Refactor: attribute 삭제

--- src/services/emotions_service.ts
+++ src/services/emotions_service.ts
@@ -2,10 +2,6 @@ import model from '../models';
 import Emotion from '../models/emotions_model';
 
 export const readAll = async () => {
-  try {
-    const emotions = await model.Emotion.findAll();
-    return emotions;
-  } catch (err) {
-    throw err;
-  }
+  const emotions: Emotion[] = await model.Emotion.findAll();
+  return emotions;
 };
