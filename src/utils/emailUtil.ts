da85b15d2362babfaf90c937fbf5e81e925e8dab
chaeppy <chaeppy108@gmail.com>
2021-01-05 14:31:02 +0900

    Style: 메일 템플릿 적용

--- src/utils/emailUtil.ts
+++ src/utils/emailUtil.ts
@@ -14,11 +14,26 @@ const transporter = nodemailer.createTransport({
 const email = {
   send: async (email: string, tempPassword: string) => {
     return await transporter.sendMail({
-      from: `"MoMo" <${process.env.NODEMAILER_USER}>`,
+      from: `"MOMO" <${process.env.NODEMAILER_USER}>`,
       to: email,
-      subject: '[MoMo] 임시 비밀번호 발급 안내',
-      text: tempPassword,
-      html: `<b>${tempPassword}</b>`,
+      subject: '[MOMO] 임시 비밀번호 발급 안내',
+      text: `안녕하세요!\n
+      책 속의 문장을 통해 깊이 있는 감정 기록을 도와주는 일기 앱, MOMO입니다.\n\n
+      아래 임시 비밀번호 8자리로 로그인해주세요.\n
+      로그인 후에는 반드시 비밀번호를 변경해주세요!\n\n
+      임시 비밀번호\n
+      ${tempPassword}\n\n
+      MOMO를 사랑해주셔서 진심으로 감사합니다.🌞\n\n
+      MOMO 드림`,
+
+      html: `안녕하세요!<br>
+      책 속의 문장을 통해 깊이 있는 감정 기록을 도와주는 일기 앱, MOMO입니다.<br><br>
+      아래 임시 비밀번호 8자리로 로그인해주세요.<br>
+      로그인 후에는 반드시 비밀번호를 변경해주세요!<br><br>
+      임시 비밀번호<br>
+      <b>${tempPassword}</b><br><br>
+      MOMO를 사랑해주셔서 진심으로 감사합니다.🌞<br><br>
+      MOMO 드림`,
     });
   },
 };
