aefb168773e60431a47d3c2b1553773794d161d9
YeongWoooo <rdd9223@gmail.com>
2021-01-09 21:30:15 +0900

    Feat: JWT 토큰 적용

--- src/routes/sentences.ts
+++ src/routes/sentences.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import { sentencesController } from '../controllers';
+import { jwt } from '../utils';
 const router = express.Router();
 
-router.get('/', sentencesController.readAll);
+router.get('/', jwt.isLoggedIn, sentencesController.readAll);
 
 export default router;
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,17 +1,18 @@
 import express from 'express';
 import { usersController } from '../controllers';
+import { jwt } from '../utils';
 const router = express.Router();
 
 router.post('/signup', usersController.signup);
 router.post('/signin/apple', usersController.signinByApple);
 router.post('/signin/social', usersController.signinBySocial);
 router.post('/signin', usersController.signin);
-router.post('/:id/password', usersController.checkPassword);
-router.get('/', usersController.readAll);
-router.get('/:id', usersController.readOne);
-router.put('/:id/alarm', usersController.updateAlarm);
-router.put('/:id/password', usersController.updatePassword);
-router.delete('/:id', usersController.deleteOne);
+router.post('/:id/password', jwt.isLoggedIn, usersController.checkPassword);
+router.get('/', jwt.isLoggedIn, usersController.readAll);
+router.get('/:id', jwt.isLoggedIn, usersController.readOne);
+router.put('/:id/alarm', jwt.isLoggedIn, usersController.updateAlarm);
+router.put('/:id/password', jwt.isLoggedIn, usersController.updatePassword);
+router.delete('/:id', jwt.isLoggedIn, usersController.deleteOne);
 router.post('/password/temp', usersController.createTempPassword);
 
 export default router;
