a7fbe9ad19a74cd8c8df996f657257984790e664
YeongWoooo <rdd9223@gmail.com>
2021-01-09 21:26:54 +0900

    Style: 디렉토리 구조 변경

--- src/routes/diaries.ts
+++ src/routes/diaries.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import { diariesController } from '../controllers';
-import { jwt } from '../utils';
 const router = express.Router();
 
-router.get('/statistics', jwt.isLoggedIn, diariesController.readStatistics);
-router.get('/recent', jwt.isLoggedIn, diariesController.readRecentOne);
-router.get('/:id', jwt.isLoggedIn, diariesController.readOne);
-router.get('/', jwt.isLoggedIn, diariesController.readAll);
-router.post('/', jwt.isLoggedIn, diariesController.create);
-router.put('/:id', jwt.isLoggedIn, diariesController.updateOne);
-router.delete('/:id', jwt.isLoggedIn, diariesController.deleteOne);
+router.get('/statistics', diariesController.readStatistics);
+router.get('/recent', diariesController.readRecentOne);
+router.get('/:id', diariesController.readOne);
+router.get('/', diariesController.readAll);
+router.post('/', diariesController.create);
+router.put('/:id', diariesController.updateOne);
+router.delete('/:id', diariesController.deleteOne);
 
 export default router;
--- src/routes/sentences.ts
+++ src/routes/sentences.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import { sentencesController } from '../controllers';
-import { jwt } from '../utils';
 const router = express.Router();
 
-router.get('/', jwt.isLoggedIn, sentencesController.readAll);
+router.get('/', sentencesController.readAll);
 
 export default router;
