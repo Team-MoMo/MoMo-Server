6fe9ed81da63c4450c13723061dc37e84f72873d
chaeppy <chaeppy108@gmail.com>
2021-01-11 00:03:43 +0900

    Refactor: 비밀번호 정규식 특수문자 허용

--- src/utils/yup.ts
+++ src/utils/yup.ts
@@ -5,21 +5,20 @@ const socialType = ['kakao', 'google', 'apple'];
 const orderType = ['depth', 'filter'];
 const emotionType = ['사랑', '행복', '슬픔', '화남', '위로', '권태', '추억', '일상'];
 
-//default : required
 const validation = {
   number: yup.number().required(),
-  optionalNumber: yup.number().notRequired(),
   email: yup.string().required().email(),
   string: yup.string().required(),
-  optionalString: yup.string().notRequired(),
   boolean: yup.boolean().required(),
   date: yup.date().required(),
+  optionalString: yup.string().notRequired(),
+  optionalNumber: yup.number().notRequired(),
 
   //user
   password: yup
     .string()
     .required()
-    .matches(/^[a-zA-Z0-9]{6,}$/),
+    .matches(/^[a-zA-Z0-9!@#$%^&*(),.?\":{}|<>_-]{6,}$/),
   socialName: yup.string().required().oneOf(socialType),
   time: yup
     .string()
