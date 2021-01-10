5862bdb0d8c02d90a4bd0ef59c76c46e85563c00
chaeppy <chaeppy108@gmail.com>
2021-01-10 19:32:06 +0900

    Style: yup 유틸 사용 방법 변경

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -5,33 +5,33 @@ import { validate } from '../middleWares';
 import yup from '../utils/yup';
 const router = express.Router();
 
-router.post('/signup', validate(yup.signupBody), usersController.signup);
-router.post('/signin/social', validate(yup.signinBySocialBody), usersController.signinBySocial);
-router.post('/signin', validate(yup.signinBody), usersController.signin);
+router.post('/signup', validate(yup.user.signupBody), usersController.signup);
+router.post('/signin/social', validate(yup.user.signinBySocialBody), usersController.signinBySocial);
+router.post('/signin', validate(yup.user.signinBody), usersController.signin);
 router.post(
   '/:id/password',
   jwt.isLoggedIn,
-  validate(yup.checkPasswordParams),
-  validate(yup.checkPasswordBody),
+  validate(yup.user.checkPasswordParams),
+  validate(yup.user.checkPasswordBody),
   usersController.checkPassword
 );
 router.get('/', jwt.isLoggedIn, usersController.readAll);
-router.get('/:id', jwt.isLoggedIn, validate(yup.readOneParams), usersController.readOne);
+router.get('/:id', jwt.isLoggedIn, validate(yup.user.readOneParams), usersController.readOne);
 router.put(
   '/:id/alarm',
   jwt.isLoggedIn,
-  validate(yup.updateAlarmParams),
-  validate(yup.updatdAlarmBody),
+  validate(yup.user.updateAlarmParams),
+  validate(yup.user.updatdAlarmBody),
   usersController.updateAlarm
 );
 router.put(
   '/:id/password',
   jwt.isLoggedIn,
-  validate(yup.updatePasswordParams),
-  validate(yup.updatePasswordBody),
+  validate(yup.user.updatePasswordParams),
+  validate(yup.user.updatePasswordBody),
   usersController.updatePassword
 );
-router.delete('/:id', jwt.isLoggedIn, validate(yup.deleteOneBody), usersController.deleteOne);
-router.post('/password/temp', validate(yup.createTempPasswordBody), usersController.createTempPassword);
+router.delete('/:id', jwt.isLoggedIn, validate(yup.user.deleteOneBody), usersController.deleteOne);
+router.post('/password/temp', validate(yup.user.createTempPasswordBody), usersController.createTempPassword);
 
 export default router;
