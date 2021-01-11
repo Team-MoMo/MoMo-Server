5ac1c3c5af2efa55012c2d4efff16001ff87ac6e
YeongWoooo <rdd9223@gmail.com>
2021-01-12 02:52:30 +0900

    Test: diary API 테스트코드 작성

--- tests/diary.test.ts
+++ tests/diary.test.ts
@@ -1,8 +1,10 @@
 import supertest from 'supertest';
 import app from '../src/app';
 import { jwt } from '../src/utils';
+import Diary from '../src/models/diaries_model';
+import dayjs from 'dayjs';
 
-let request: supertest.SuperTest<supertest.Test>, token: string;
+let request: supertest.SuperTest<supertest.Test>, token: string, diaryInfo: Diary;
 
 const BASE_URL = '/diaries';
 
@@ -13,30 +15,11 @@ describe('[DIARY] API TEST', () => {
     done();
   });
 
-  afterAll(async () => {});
-
   test('[GET] /', async (done) => {
     const res = await request.get('/');
     expect(res).toBeDefined();
     expect(res.status).toEqual(200);
     expect(res.type).toEqual('application/json');
-    // expect(res.body.data.constructor).toEqual(Array);
-    // expect(res.body.data[0]).toMatchObject({
-    //   id: expect.any(Number),
-    //   name: expect.any(String),
-    //   email: expect.any(String),
-    //   birthdate: null,
-    //   sex: null,
-    //   phone: expect.any(String),
-    //   point: expect.any(Number),
-    //   isAgreeTerms: expect.any(Boolean),
-    //   isAgreeUsePersonalData: expect.any(Boolean),
-    //   isEmailAuth: expect.any(Boolean),
-    //   isPhoneAuth: expect.any(Boolean),
-    //   isDeleted: expect.any(Boolean),
-    //   createdAt: expect.any(String),
-    //   updatedAt: expect.any(String),
-    // });
     done();
   });
 
@@ -50,6 +33,116 @@ describe('[DIARY] API TEST', () => {
     expect(res.status).toEqual(200);
     expect(res.type).toEqual('application/json');
     expect(res.body.message).toEqual('일기 전체 조회 성공');
+    done();
+  });
+
+  test(`[GET] ${BASE_URL}?order=filter&userId=1&year=2020&month=11&day=1`, async (done) => {
+    const res = await request
+      .get(BASE_URL)
+      .query({ userId: '1', year: '2020', month: '11', order: 'filter', day: '1' })
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('일기 전체 조회 성공');
+    done();
+  });
+
+  test(`[GET] ${BASE_URL}?order=filter&userId=1&year=2020&month=11&emotionId=1&depth=1`, async (done) => {
+    const res = await request
+      .get(BASE_URL)
+      .query({ userId: '1', year: '2020', month: '11', order: 'filter', emotionId: '1', depth: '1' })
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('일기 전체 조회 성공');
+    done();
+  });
+
+  test(`[GET] ${BASE_URL}/recent`, async (done) => {
+    const res = await request
+      .get(`${BASE_URL}/recent`)
+      .query({ userId: '1' })
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('일기 조회 성공');
+    done();
+  });
+
+  test(`[GET] ${BASE_URL}/statistics`, async (done) => {
+    const res = await request
+      .get(`${BASE_URL}/statistics`)
+      .query({ userId: '1', year: '2020', month: '1' })
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('일기통계 조회 성공');
+    done();
+  });
+
+  test(`[POST] ${BASE_URL}`, async (done) => {
+    const res = await request
+      .post(BASE_URL)
+      .send({
+        contents: 'dummmy',
+        userId: 1,
+        emotionId: 1,
+        sentenceId: 1,
+        wroteAt: dayjs().format(),
+        depth: 1,
+      })
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(201);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('일기 생성 성공');
+    diaryInfo = res.body.data;
+    done();
+  });
+
+  test(`[GET] ${BASE_URL}/:id`, async (done) => {
+    const res = await request
+      .get(`${BASE_URL}/${diaryInfo.id}`)
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('일기 조회 성공');
+    done();
+  });
+
+  test(`[PUT] ${BASE_URL}/:id`, async (done) => {
+    const res = await request
+      .put(`${BASE_URL}/${diaryInfo.id}`)
+      .send({ contents: 'dummy2', wroteAt: dayjs().format(), depth: 2 })
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('일기 수정 성공');
+    done();
+  });
+
+  test(`[DELETE] ${BASE_URL}/:id`, async (done) => {
+    const res = await request
+      .delete(`${BASE_URL}/${diaryInfo.id}`)
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('일기 삭제 성공');
     done();
   });
 });
