9e40ebefe37f8f976925e507ca756151f9e49557
yeonghyeonSeok <syh4834@naver.com>
2021-01-12 18:23:00 +0900

    Feat: 온보딩문장 조회 API

--- src/routes/sentences.ts
+++ src/routes/sentences.ts
@@ -6,6 +6,7 @@ import yup from '../utils/yup';
 const router = express.Router();
 
 router.get('/', jwt.isLoggedIn, validate(yup.sentence.readAllQuery), sentencesController.readAll);
+router.get('/onboarding', validate(yup.sentence.readAllOnboarding), sentencesController.readAllOnboarding);
 router.post('/', validate(yup.sentence.create), sentencesController.create);
 
 export default router;
