b0bde6d701df1bca737bd57bedd90c718e1c2eea
chaeppy <chaeppy108@gmail.com>
2021-01-12 14:58:40 +0900

    Test: 회원가입,로그인,비밀번호,회원삭제 테스트 추가

--- tests/user.test.ts
+++ tests/user.test.ts
@@ -3,20 +3,62 @@ import app from '../src/app';
 import { jwt } from '../src/utils';
 require('mysql2/node_modules/iconv-lite').encodingExists('foo');
 
-let request: supertest.SuperTest<supertest.Test>, token: string;
+let request: supertest.SuperTest<supertest.Test>, token: string, userId: number;
 
 const BASE_URL = '/users';
+const randomString = Math.random().toString(36).slice(5);
 
 describe('[USER] API TEST', () => {
   beforeAll(async (done) => {
     request = supertest(app);
-    token = jwt.sign({ id: 1 }).token;
     done();
   });
 
   afterAll(async () => {});
 
-  test('[GET] /', async (done) => {
+  test(`[GET] ${BASE_URL}/signup?email`, async (done) => {
+    const res = await request
+      .get(`${BASE_URL}/signup`)
+      .query({ email: `${randomString}@gmoil.com` })
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('사용 가능한 이메일입니다.');
+    done();
+  });
+
+  test(`[POST] ${BASE_URL}/signup`, async (done) => {
+    const res = await request
+      .post(`${BASE_URL}/signup`)
+      .send({ email: `${randomString}@gmoil.com`, password: randomString })
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(201);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('회원가입 성공');
+    done();
+  });
+
+  test(`[POST] ${BASE_URL}/social`, async (done) => {
+    done();
+  });
+
+  test(`[POST] ${BASE_URL}/signin`, async (done) => {
+    const res = await request
+      .post(`${BASE_URL}/signin`)
+      .send({ email: `${randomString}@gmoil.com`, password: randomString })
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('로그인 성공');
+    userId = res.body.data.user.id;
+    token = jwt.sign({ id: userId }).token;
+    done();
+  });
+
+  test(`[GET] ${BASE_URL}/`, async (done) => {
     const res = await request.get(BASE_URL).set('Authorization', token).set('Accept', 'application/json');
     expect(res).toBeDefined();
     expect(res.status).toEqual(200);
@@ -25,9 +67,9 @@ describe('[USER] API TEST', () => {
     done();
   });
 
-  test('[GET] /:id', async (done) => {
+  test(`[GET] ${BASE_URL}/:id`, async (done) => {
     const res = await request
-      .get(BASE_URL + '/1')
+      .get(`${BASE_URL}/${userId}`)
       .set('Authorization', token)
       .set('Accept', 'application/json');
     expect(res).toBeDefined();
@@ -37,9 +79,35 @@ describe('[USER] API TEST', () => {
     done();
   });
 
-  test('[PUT] /:id/alarm(알람설정변경)', async (done) => {
+  test(`[POST] ${BASE_URL}/:id/password`, async (done) => {
     const res = await request
-      .put(BASE_URL + '/1/alarm')
+      .post(`${BASE_URL}/${userId}/password`)
+      .send({ password: randomString })
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('비밀번호가 일치합니다');
+    done();
+  });
+
+  test(`[PUT] ${BASE_URL}/:id/password`, async (done) => {
+    const res = await request
+      .put(`${BASE_URL}/${userId}/password`)
+      .send({ newPassword: `${randomString}123` })
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('비밀번호 수정 성공');
+    done();
+  });
+
+  test(`[PUT] ${BASE_URL}/:id/alarm (알람설정변경)`, async (done) => {
+    const res = await request
+      .put(`${BASE_URL}/${userId}/alarm`)
       .send({ isAlarmSet: true })
       .set('Authorization', token)
       .set('Accept', 'application/json');
@@ -50,9 +118,9 @@ describe('[USER] API TEST', () => {
     done();
   });
 
-  test('[PUT] /:id/alarm(알람시간변경)', async (done) => {
+  test(`[PUT] ${BASE_URL}/:id/alarm (알람시간변경)`, async (done) => {
     const res = await request
-      .put(BASE_URL + '/1/alarm')
+      .put(`${BASE_URL}/${userId}/alarm`)
       .send({ isAlarmSet: true, alarmTime: '08:00' })
       .set('Authorization', token)
       .set('Accept', 'application/json');
@@ -60,6 +128,30 @@ describe('[USER] API TEST', () => {
     expect(res.status).toEqual(200);
     expect(res.type).toEqual('application/json');
     expect(res.body.message).toEqual('알람 수정 성공');
+    done();
+  });
+
+  test(`[POSt] ${BASE_URL}/password/temp`, async (done) => {
+    const res = await request
+      .post(`${BASE_URL}/password/temp`)
+      .send({ email: `${randomString}@gmoil.com` })
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('임시 비밀번호 수정 성공');
+    done();
+  });
+
+  test(`[DELETE] ${BASE_URL}/:id`, async (done) => {
+    const res = await request
+      .delete(`${BASE_URL}/${userId}`)
+      .set('Authorization', token)
+      .set('Accept', 'application/json');
+    expect(res).toBeDefined();
+    expect(res.status).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('회원 삭제 성공');
     done();
   });
 });
