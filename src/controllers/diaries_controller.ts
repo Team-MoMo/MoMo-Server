ad2f351d5784a00c8610d2443416215a63402876
YeongWoooo <rdd9223@gmail.com>
2021-01-14 21:41:02 +0900

    Refactor: 가장 최근에 일기작성안한 날짜 조회

--- src/controllers/diaries_controller.ts
+++ src/controllers/diaries_controller.ts
@@ -56,9 +56,13 @@ export const readRecentOne = async (req: Request, res: Response) => {
     if (!recentDiaryInfo) {
       return res.status(statusCode.BAD_REQUEST).json(resJson.fail(resMessage.NO_X(DIARY)));
     }
-    return res.status(statusCode.OK).json(resJson.success(resMessage.X_READ_SUCCESS(DIARY), recentDiaryInfo));
+    return res
+      .status(statusCode.OK)
+      .json(resJson.success(resMessage.X_READ_SUCCESS(`${DIARY} 최근 미작성 날짜`), recentDiaryInfo));
   } catch (err) {
-    return res.status(statusCode.INTERNAL_SERVER_ERROR).json(resJson.fail(resMessage.X_READ_FAIL(DIARY), err));
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .json(resJson.fail(resMessage.X_READ_FAIL(`${DIARY} 최근 미작성 날짜`), err));
   }
 };
 
