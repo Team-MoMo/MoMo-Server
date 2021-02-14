c17efe1f897ae4240b073143004a1214bf17a59f
chaeppy <chaeppy108@gmail.com>
2021-02-15 00:45:12 +0900

    Feat: scheduler controller, service삭제

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -2,4 +2,3 @@ export * as usersService from './users_service';
 export * as diariesService from './diaries_service';
 export * as sentencesService from './sentences_service';
 export * as emotionsService from './emotions_service';
-export * as schedulerService from './scheduler_service';
