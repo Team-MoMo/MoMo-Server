b64c81fdcc0d5e875683ba2df879477c20174762
yeonghyeonSeok <syh4834@naver.com>
2021-02-18 03:47:26 +0900

    Fix: 추천문장 삭제 시 isDeleted = null

--- server/src/services/scheduler_service.ts
+++ server/src/services/scheduler_service.ts
@@ -1,13 +1,24 @@
 import { Op } from 'sequelize';
 import model from '../models';
 import dayjs from 'dayjs';
+import UsersRecommendedSentences from '../models/users_recommended_sentences_model';
 
-export const deleteAllYesterday = async (yesterday: dayjs.Dayjs) => {
+export const readAllYesterday = async (yesterday: dayjs.Dayjs) => {
   try {
-    await model.UsersRecommendedSentences.destroy({
+    const yesterdaySentences: UsersRecommendedSentences[] = await model.UsersRecommendedSentences.findAll({
       where: {
         createdAt: { [Op.lt]: yesterday.format('YYYY-MM-DD HH:mm') },
       },
     });
+    return yesterdaySentences;
+  } catch (err) {}
+};
+
+export const deleteAllYesterday = async (yesterdaySentences: UsersRecommendedSentences[]) => {
+  try {
+    yesterdaySentences.forEach(async (element) => {
+      await element.destroy();
+    });
+    return;
   } catch (err) {}
 };
