1248310483e6467726f219ff811461a559433a5e
yeonghyeonSeok <syh4834@naver.com>
2021-02-21 21:10:49 +0900

    Style: try ~ catch문 삭제

--- server/src/services/scheduler_service.ts
+++ server/src/services/scheduler_service.ts
@@ -4,21 +4,17 @@ import dayjs from 'dayjs';
 import UsersRecommendedSentences from '../models/users_recommended_sentences_model';
 
 export const readAllYesterday = async (yesterday: dayjs.Dayjs) => {
-  try {
-    const yesterdaySentences: UsersRecommendedSentences[] = await model.UsersRecommendedSentences.findAll({
-      where: {
-        createdAt: { [Op.lt]: yesterday.format('YYYY-MM-DD HH:mm') },
-      },
-    });
-    return yesterdaySentences;
-  } catch (err) {}
+  const yesterdaySentences: UsersRecommendedSentences[] = await model.UsersRecommendedSentences.findAll({
+    where: {
+      createdAt: { [Op.lt]: yesterday.format('YYYY-MM-DD HH:mm') },
+    },
+  });
+  return yesterdaySentences;
 };
 
 export const deleteAllYesterday = async (yesterdaySentences: UsersRecommendedSentences[]) => {
-  try {
-    yesterdaySentences.forEach(async (element) => {
-      await element.destroy();
-    });
-    return;
-  } catch (err) {}
+  yesterdaySentences.forEach(async (element) => {
+    await element.destroy();
+  });
+  return;
 };
