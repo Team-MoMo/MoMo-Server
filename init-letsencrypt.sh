ec5622847090fc25bd6bddec611652f80cb9dc9e
YeongWoooo <rdd9223@gmail.com>
2021-06-27 17:36:48 +0900

    Fix: .shop도메인도 접근 가능하도록 설정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(momodiary.ga www.momodiary.ga)
+domains=(momodiary.ga www.momodiary.ga momodiary.shop www.momodiary.shop)
 rsa_key_size=4096
 data_path="./certbot"
 email="rdd9223@gmail.com" # Adding a valid address is strongly recommended
